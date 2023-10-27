5753d42e0919ec78717f710c90e1a01e9646cca2
jpoechill <poamrong@rith.com>
2023-10-27 00:29:42 -0700

    complete lesson

--- src/script.js
+++ src/script.js
@@ -1,28 +1,67 @@
 import * as THREE from 'three'
 
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+/**
+ * Objects
+ */
 
-// Sizes
+const group = new THREE.Group()
+
+group.position.y = 1
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 'orange' })
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(2, .21, 1),
+    new THREE.MeshBasicMaterial({ color: 'blue' })
+)
+
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, .21, 2),
+    new THREE.MeshBasicMaterial({ color: 'red' })
+)
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
+
+scene.add(group)
+
+
+// Axes helpers
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
 const sizes = {
     width: 800,
     height: 600
 }
 
-// Camera
+/**
+ * Camera
+ */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
 
-// Renderer
+
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('canvas.webgl')
+    canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
